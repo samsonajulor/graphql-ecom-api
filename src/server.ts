6356193c542652d8d8c377a6ad114a76e5ffa493
Samson Ajulor <samson@esoftresponse.com>
2022-07-19 23:00:47 +0100

    completed the api

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import typeDefs from './schema';
 import { Category } from './resolvers/Category';
 import { Mutation } from './resolvers/Mutation';
 import { Product } from './resolvers/Product';
+import { Query } from './resolvers/Query';
 import db from './db';
 
 const server: ApolloServer = new ApolloServer({
   typeDefs,
   resolvers: {
+    Query,
     Mutation,
     Product,
     Category,
