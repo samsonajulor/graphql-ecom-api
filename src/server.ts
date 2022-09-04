b95c53a307c42b9b15570ad7ca58e0b90c719240
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-09-04 12:20:17 +0100

    added playground

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import { ApolloServer } from 'apollo-server';
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground
+} from "apollo-server-core";
 import typeDefs from './schema';
 import { Category } from './resolvers/Category';
 import { Mutation } from './resolvers/Mutation';
@@ -17,6 +20,11 @@ const server: ApolloServer = new ApolloServer({
   context: {
     db
   },
+  csrfPrevention: true,
+  cache: "bounded",
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
 });
 
 server.listen(process.env.PORT || 5000).then(({ url }) => {
